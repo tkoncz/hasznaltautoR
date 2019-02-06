d790dc39d643b4efb91ec7c3d636c128f99d9c3b
tomiaJO <t.koncz.com>
2019-02-06 20:43:59 +0100

    :recycle: refactor of getAdDetailsFromUrl: move parsing step to their own functions

--- R/car_ad_details.R
+++ R/car_ad_details.R
@@ -19,39 +19,57 @@
 getAdDetailsFromUrl <- function(ad_url) {
     ad_html <- xml2::read_html(ad_url)
 
-    ad_code <- gsub(".*-(\\d{1,15})$", "\\1", ad_url)      
+    ad_code            <- getAdCode(ad_url)      
+    ad_title           <- getAdTitle(ad_html)
+    ad_description     <- getAdDescription(ad_html)
+    ad_main_attributes <- getAdMainAttributes(ad_html)
 
-    ad_title <- ad_html %>%
+    ad_table <- purrr::flatten(list(
+        "Hirdetéskód" = ad_code, 
+        "Cím"         = ad_title, 
+        "Leírás"      = ad_description,
+        "URL"         = ad_url, 
+        ad_main_attributes
+    )) %>% dplyr::as_tibble()
+
+    ad_table
+}
+
+getAdCode <- function(ad_url) {
+    gsub(".*-(\\d{1,15})$", "\\1", ad_url)
+}
+
+getAdTitle <- function(ad_html) {
+    ad_html %>%
         rvest::html_nodes("h1") %>%
         rvest::html_text() %>%
         {if(length(.) == 0) NA_character_ else .}
+}
 
-    ad_description <- ad_html %>%
+getAdDescription <- function(ad_html) {
+    ad_html %>%
         rvest::html_nodes(xpath = "//div[@class='leiras']/div") %>%
         rvest::html_text() %>%
         {if(length(.) == 0) NA_character_ else .}
+}
 
-    ad_main_attributes <- ad_html %>%
-        rvest::html_nodes(
-            xpath = "//table[@class='hirdetesadatok']//td[@class='bal pontos']"
-        ) %>%
-        rvest::html_text()
-    ad_main_attributes <- gsub(":", "", ad_main_attributes)
-    
+getAdMainAttributes <- function(ad_html) {
     ad_main_attribute_values <- ad_html %>%
         rvest::html_nodes(
             xpath = "//table[@class='hirdetesadatok']//td[not (@class='bal pontos')]"
         ) %>%
         rvest::html_text()
 
-    ad_table <- list(
-        ad_code, ad_title, ad_description, ad_url, ad_main_attribute_values
-    ) %>%
-        purrr::flatten() %>%
-        purrr::set_names(c(
-            "Hirdetéskód", "Cím", "Leírás", "URL", ad_main_attributes
-        )) %>%
-        dplyr::as_tibble()
+    ad_main_attributes <- ad_html %>%
+        rvest::html_nodes(
+            xpath = "//table[@class='hirdetesadatok']//td[@class='bal pontos']"
+        ) %>%
+        rvest::html_text()
 
-    ad_table
+    ad_main_attributes <- gsub(":", "", ad_main_attributes)
+    ad_main_attribute_values <- purrr::set_names(
+        ad_main_attribute_values, ad_main_attributes
+    )
+
+    ad_main_attribute_values
 }
